634b717c2677e3291b32bfee61bfa28283818f76
Nathanael-R <nathanielwealth20@gmail.com>
2023-11-16 21:52:29 +0000

    Array now sorts

--- client/components/List.jsx
+++ client/components/List.jsx
@@ -24,12 +24,12 @@ const List = () => {
   }, []); // Only run the effect once on component mount
 
   function scoreSort (a, b) {
-    return a-b
+    return b.score-a.score
   }
   return (
     <div className="p-4">
       {
-        data.sort().map((player, idx) => {
+        data.sort(scoreSort).map((player, idx) => {
           return (
             <Item key={idx} score={player?.score} name={player?.name}/>
           )
